4f441c1d795194ec21102e18ec8d8477cc339a2d
MoniRogi <kamoma123@gmail.com>
2023-03-15 17:26:57 +0100

    Zadanie domowe nr 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe nr 4
+
+
+string name = "Ew";
+string sex = "Kobiet";
+int age = 63;
+
+
+
+if (sex == "Kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (age < 30)
+{
+    Console.WriteLine("Młoda kobieta.");
+}
+else
+{
+    Console.WriteLine("Starszy mężczyzna.");
+}
