ea65a416e3f199d2b3681ac81b0bf817e63d0075
MoniRogi <kamoma123@gmail.com>
2023-05-01 20:14:15 +0200

    Zadanie nr 15

--- ChallengeApp/ChallengeApp/Supervisor.cs
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -1,19 +1,15 @@
-﻿
-using System.Diagnostics;
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
-    public class Employee : IEmployee
+    public class Supervisor : IEmployee
     {
         private List<float> grades = new List<float>();
 
-        public Employee(string name, string surname, string sex)
+        public Supervisor(string name, string surname, string sex)
         {
             this.Name = name;
             this.Surname = surname;
             this.Sex = sex;
         }
-
         public string Name { get; private set; }
 
         public string Surname { get; private set; }
@@ -42,51 +38,78 @@ namespace ChallengeApp
         {
             float gradeIsFloat = grade;
             this.AddGrade(gradeIsFloat);
-        }
+        }       
 
         public void AddGrade(char grade)
         {
-
-            switch (grade)
-            {
-                case 'A':
-                case 'a':
-                    this.grades.Add(100);
-                    break;
-                case 'B':
-                case 'b':
-                    this.grades.Add(80);
-                    break;
-                case 'C':
-                case 'c':
-                    this.grades.Add(60);
-                    break;
-                case 'D':
-                case 'd':
-                    this.grades.Add(40);
-                    break;
-                case 'E':
-                case 'e':
-                    this.grades.Add(20);
-                    break;
-                default:
-                    throw new Exception("Wrong Letter");
-            }
+            var gradeIsFloat = (float)grade;
+            this.AddGrade(gradeIsFloat);
         }
 
         public void AddGrade(string grade)
         {
-            if (float.TryParse(grade, out float result))
+            switch (grade)
             {
-                this.AddGrade(result);
-            }
-            else if (char.TryParse(grade, out char res))
-            {
-                this.AddGrade(res);
-            }
-            else
-            {
-                throw new Exception("String is not float or char");
+                case "6":
+                    this.grades.Add(100);
+                    break;
+                case "6+":
+                case "-6":
+                    this.grades.Add(95);
+                    break;
+                case "5+":
+                case "+5":
+                    this.grades.Add(85);
+                    break;
+                case "5":
+                    this.grades.Add(80);
+                    break;
+                case "5-":
+                case "-5":
+                    this.grades.Add(75);
+                    break;
+                case "4+":
+                case "+4":
+                    this.grades.Add(65);
+                    break;
+                case "4":
+                    this.grades.Add(60);
+                    break;
+                case "4-":
+                case "-4":
+                    this.grades.Add(55);
+                    break;
+                case "3+":
+                case "+3":
+                    this.grades.Add(45);
+                    break;
+                case "3":
+                    this.grades.Add(40);
+                    break;
+                case "-3":
+                case "3-":                
+                    this.grades.Add(35);
+                    break;
+                case "2+":
+                case "+2":
+                    this.grades.Add(25);
+                    break;
+                case "2":
+                    this.grades.Add(20);
+                    break;
+                case "2-":
+                case "-2":
+                    this.grades.Add(15);
+                    break;
+                case "1+":
+                case "+1":
+                    this.grades.Add(5);
+                    break;
+                case "1":
+                    this.grades.Add(0);
+                    break;
+                default:
+                    throw new Exception("Wrong grade (write 1-6)");
             }
         }
 
@@ -130,10 +153,7 @@ namespace ChallengeApp
 
             return statistics;
         }
-
     }
-
+    
+    
 }
-
-
-
