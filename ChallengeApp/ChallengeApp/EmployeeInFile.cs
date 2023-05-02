6e39cf1aefd66d4da1ab5bb0c8efd60aabba4691
MoniRogi <kamoma123@gmail.com>
2023-05-03 00:31:36 +0200

    Zadanie nr 16

---
+++ ChallengeApp/ChallengeApp/EmployeeInFile.cs
@@ -0,0 +1,169 @@
+﻿namespace ChallengeApp
+{
+    public class EmployeeInFile : EmployeeBase
+    {
+        private const string filename = "grades.txt";
+        public EmployeeInFile(string name, string surname, string sex)
+            : base(name, surname, sex)
+        {
+        }
+
+        public override void AddGrade(float grade)
+        {
+            using (var writer = File.AppendText(filename))
+            {
+                if (grade >= 0 && grade <= 100)
+
+                {
+                    writer.WriteLine(grade);
+                }
+
+                else
+                {
+                    throw new Exception("invalid grade value");
+                }
+            }
+        }
+
+        public override void AddGrade(double grade)
+        {
+            using (var writer = File.AppendText(filename))
+            {
+                writer.WriteLine(grade);
+            }
+        }
+
+        public override void AddGrade(int grade)
+        {
+            using (var writer = File.AppendText(filename))
+            {
+                writer.WriteLine(grade);
+            }
+        }
+
+        public override void AddGrade(char grade)
+        {
+            using (var writer = File.AppendText(filename))
+            {
+                switch (grade)
+                {
+                    case 'A':
+                    case 'a':
+                        this.AddGrade(100);
+                        break;
+                    case 'B':
+                    case 'b':
+                        this.AddGrade(80);
+                        break;
+                    case 'C':
+                    case 'c':
+                        this.AddGrade(60);
+                        break;
+                    case 'D':
+                    case 'd':
+                        this.AddGrade(40);
+                        break;
+                    case 'E':
+                    case 'e':
+                        this.AddGrade(20);
+                        break;
+                    default:
+                        throw new Exception("Wrong Letter");
+                }
+            }
+
+        }
+
+        public override void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                using (var writer = File.AppendText(filename))
+                {
+                    writer.WriteLine(result);
+                }
+            }
+            else if (char.TryParse(grade, out char res))
+            {
+                using (var writer = File.AppendText(filename))
+                {
+                    writer.WriteLine(res);
+                }
+            }
+            else
+            {
+                throw new Exception("String is not float or char");
+            }
+        }
+
+        public override Statistics GetStatistics()
+        {
+            var gradesFromFile = this.ReadGradesFromFile();
+            var result = this.CountStatistics(gradesFromFile);
+            return result;
+        }
+
+        private List<float> ReadGradesFromFile()
+        {
+            var grades = new List<float>();
+            if (File.Exists($"{filename}"))
+            {
+
+                using (var reader = File.OpenText($"{filename}"))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        var number = float.Parse(line);
+                        grades.Add(number);
+                        line = reader.ReadLine();
+                    }
+
+                }
+            }
+            return grades;
+        }
+        private List<float> CountStatistics(List<float> grades)
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                if (grade >= 0)
+                {
+                    statistics.Max = Math.Max(statistics.Max, grade);
+                    statistics.Min = Math.Min(statistics.Min, grade);
+                    statistics.Average += grade;
+                }
+            }
+            statistics.Average /= grades.Count;
+
+
+            switch (statistics.Average)
+            {
+                case var average when average >= 80:
+                    statistics.AverageLetter = 'A';
+                    break;
+                case var average when average >= 60:
+                    statistics.AverageLetter = 'B';
+                    break;
+                case var average when average >= 40:
+                    statistics.AverageLetter = 'C';
+                    break;
+                case var average when average >= 20:
+                    statistics.AverageLetter = 'D';
+                    break;
+                default:
+                    statistics.AverageLetter = 'E';
+                    break;
+            }
+
+            return statistics;
+        }
+
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -6,50 +6,62 @@ Console.WriteLine("Witamy w Programie XYZ do oceny Pracowników");
 Console.WriteLine("===========================================");
 Console.WriteLine();
 Console.WriteLine("Podaj dane pracownika: ");
-Console.WriteLine("Wpisz imię: ");
-var name = Console.ReadLine();
-Console.WriteLine("Wpisz nazwisko: ");
-var surname = Console.ReadLine();
-Console.WriteLine("Wpisz płeć (K/M): ");
-var sex = Console.ReadLine();
+//Console.WriteLine("Wpisz imię: ");
+//var name = Console.ReadLine();
+//Console.WriteLine("Wpisz nazwisko: ");
+//var surname = Console.ReadLine();
+//Console.WriteLine("Wpisz płeć (K/M): ");
+//var sex = Console.ReadLine();
 
-var employee = new Employee(name, surname, sex);
+var employee = new EmployeeInFile("Monika", "hhg", "k");
 
-Console.WriteLine("Podaj ocenę pracownika: ");
-var input = Console.ReadLine();
+employee.AddGrade("5");
+employee.AddGrade("A");
+employee.AddGrade("e");
+employee.AddGrade("45");
 
-try
-{
-    employee.AddGrade(input);
-}
-catch (Exception e)
-{
-    Console.WriteLine($"Exception catched: {e.Message}");
-}
+Console.WriteLine();
+var statisticsEmployeeInFile = employee.GetStatistics();
+Console.WriteLine($"Average: {statisticsEmployeeInFile.Average}");
+Console.WriteLine($"Min: {statisticsEmployeeInFile.Min}");
+Console.WriteLine($"Max: {statisticsEmployeeInFile.Max}");
+Console.WriteLine($"AverageLetter: {statisticsEmployeeInFile.AverageLetter}");
 
-while (true)
-{
-    Console.WriteLine("Podaj kolejną ocenę pracownika (jeśli chcesz wyjść naciśnij 'q'): ");
-    input = Console.ReadLine();
-    if (input == "q")
-    {
-        Console.WriteLine("Oto wyniki podanego pracownika: "); 
-        break;        
-    }
+//Console.WriteLine("Podaj ocenę pracownika: ");
+//var input = Console.ReadLine();
 
-    try
-    {
-        employee.AddGrade(input);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Exception catched: {e.Message}");
-    }
-  
-}
+//try
+//{
+//    employee.AddGrade(input);
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine($"Exception catched: {e.Message}");
+//}
 
-var ststistics = employee.GetStatistics();
-Console.WriteLine($"Average: {ststistics.Average}");
-Console.WriteLine($"Min: {ststistics.Min}");
-Console.WriteLine($"Max: {ststistics.Max}");
-Console.WriteLine($"AverageLetter: {ststistics.AverageLetter}");
+//while (true)
+//{
+//    Console.WriteLine("Podaj kolejną ocenę pracownika (jeśli chcesz wyjść naciśnij 'q'): ");
+//    input = Console.ReadLine();
+//    if (input == "q")
+//    {
+//        Console.WriteLine("Oto wyniki podanego pracownika: "); 
+//        break;        
+//    }
+
+//    try
+//    {
+//        employee.AddGrade(input);
+//    }
+//    catch (Exception e)
+//    {
+//        Console.WriteLine($"Exception catched: {e.Message}");
+//    }
+
+//}
+
+//var ststistics = employee.GetStatistics();
+//Console.WriteLine($"Average: {ststistics.Average}");
+//Console.WriteLine($"Min: {ststistics.Min}");
+//Console.WriteLine($"Max: {ststistics.Max}");
+//Console.WriteLine($"AverageLetter: {ststistics.AverageLetter}");
